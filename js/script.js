7bfe3ac5ea9a7607af8c338aca60c51117432ee1
without-fantasy <artem-merzlykov@mail.ru>
2023-09-12 19:46:30 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-let el = document.getElementById('running_line');alert(el);
+let el = document.getElementById('running_line');
 let text = el.innerHTML;
 
 setInterval(function ()
 {
     text = text[text.length-1] + text.substring(0, text.length-1);
     el.innerHTML = text;
-}, 100);
+}, 700);
