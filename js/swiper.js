00a010e2ec5112a68d9115881dc66413343b2029
denis.bosyi <denis.bosiy@gmail.com>
2023-09-14 00:34:25 +0300

    Правки Дениса

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,9 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs';
+
+const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    preloadImages: false,
     loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    lazy: true,
+    grabCursor: true
 });
